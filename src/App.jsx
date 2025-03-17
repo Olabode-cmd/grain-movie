707da04c6ded65f9841bdc813972f6985dfe0724
Olabode-cmd <olabodebalogun80@gmail.com>
2025-03-17 10:58:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import media from './data/media'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-green-600 text-3xl mt-20'>Blank page</h1>
+
+      {/* {media.map((movie) => (
+        <div>
+          <img src={movie.poster} alt={movie.title} />
+          <h1>{movie.title}</h1>
+          <p className="text-md">{movie.description}</p>
+        </div>
+      ))} */}
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route  } from 'react-router'
+import './index.css'
+import App from './App.jsx'
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
