22728eec3c1e0ae6ddbc23f10603770ebb2f8639
Olabode-cmd <olabodebalogun80@gmail.com>
2025-03-19 09:47:19 +0100

    adjusted hero, search section, banner links added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = ()=>{
     return (
       <div className="bg-black py-5">
-        <div className="flex items-center justify-between max-w-[96rem] px-4 mx-auto">
+        <div className="flex items-center justify-between max-w-7xl 2xl:max-w-[96rem] px-4 mx-auto">
           <div className="text-white text-xl">Prime video</div>
           <button className="bg-white hover:bg-blue-600 duration-150 text-black hover:text-white py-2 px-2 rounded-md">Get started</button>
         </div>
