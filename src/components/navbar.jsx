69478747f44e8b3503e50732a1407606bd50c50a
Chanel-debb <happy4chanel@gmail.com>
2025-03-19 06:46:36 -0700

    search bar, and created a category

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = ()=>{
     return (
-      <div className="bg-black py-5">
+      <div className="bg-black py-5 fixed top-0 w-full z-30">
         <div className="flex items-center justify-between max-w-7xl 2xl:max-w-[96rem] px-4 mx-auto">
           <div className="text-white text-xl">Prime video</div>
           <button className="bg-white hover:bg-blue-600 duration-150 text-black hover:text-white py-2 px-2 rounded-md">Get started</button>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route  } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import About from './pages/about.jsx'
 
 const root = document.getElementById("root");
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(root).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
